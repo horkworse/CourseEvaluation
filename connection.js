be1d2abeef30e9510bb86d954ae3af027d2079c2
Tzex <tzex@yandex.ru>
2021-04-08 16:00:18 -0700

    Удалил index.mjs; изменил connection.js

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "82.202.172.211",
   port: "3316",
   user: "u106227_ca",
-  //database: "u106227_ce",
+  database: "u106227_ce",
   password: "bW1cI0oG4xeW7h"
 });
  connection.connect(function(err){
@@ -15,6 +15,10 @@ const connection = mysql.createConnection({
       console.log("Подключение к серверу MySQL успешно установлено");
     }
  });
+ connection.execute("select Name, SecondName from Users;",
+  function(err, results, fields) {
+    console.log(JSON.stringify(results)); // собственно данные
+});
  connection.end(function(err) {
   if (err) {
     return console.log("Ошибка: " + err.message);
