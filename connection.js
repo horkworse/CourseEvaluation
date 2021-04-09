5b6d283eaec0d437beb678ab05fce09f5a8bc041
Tzex <tzex@yandex.ru>
2021-04-09 03:28:03 -0700

    create connection to mysql, create methods to work with

--- connection.js
+++ connection.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql2");
-
 const connection = mysql.createConnection({
   host: "82.202.172.211",
   port: "3316",
@@ -7,21 +6,29 @@ const connection = mysql.createConnection({
   database: "u106227_ce",
   password: "bW1cI0oG4xeW7h"
 });
- connection.connect(function(err){
+
+
+exports.connection__connect = () => {connection.connect(function(err) {
     if (err) {
-      return console.error("Ошибка: " + err.message);
+      console.log("Ошибка: " + err.message);
     }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
- connection.execute("select Name, SecondName from Users;",
+    console.log("Подключение к серверу MySQL успешно установлено");
+ })};
+ 
+
+exports.connection__execute = () => {connection.execute("select Surname, First_name from Users;",
   function(err, results, fields) {
-    console.log(JSON.stringify(results)); // собственно данные
-});
- connection.end(function(err) {
+    if (err) {
+      console.log("Ошибка: " + err.message);
+    }
+    console.log(JSON.stringify(results));
+    return JSON.stringify(results);
+})};
+
+
+exports.connection__end = () => {connection.end(function(err) {
   if (err) {
-    return console.log("Ошибка: " + err.message);
+    console.log("Ошибка: " + err.message);
   }
   console.log("Подключение закрыто");
-});
+})};
