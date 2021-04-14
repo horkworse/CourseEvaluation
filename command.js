ccdbf864f98e61469e731ea8a11054488df9c365
Honk <lennoninmyheart@gmail.com>
2021-04-14 13:41:54 +0500

    Surveys (Страница опросов)
    html + css + js ( js нерабочий :\ )

---
+++ command.js
@@ -0,0 +1,11 @@
+function open() {
+	document.getElementById('open_case').setAttribute("style", "opacity:1; transition: 1s; height: 100%;");
+	document.getElementById('open_fnct').setAttribute("style", "display:none;");
+	document.getElementById('close_fnct').setAttribute("style", "display:block;");
+}
+
+function close() {
+	document.getElementById('open_case').setAttribute("style", "display:none;");
+	document.getElementById('close_fnct').setAttribute("style", "display:none;");
+	document.getElementById('open_fnct').setAttribute("style", "display:block");
+}
