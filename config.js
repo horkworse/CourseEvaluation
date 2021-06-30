4c63bddf30c820324025acd9553b7858ed25d549
Tzex <64221012+Tzex@users.noreply.github.com>
2021-06-30 16:51:52 +0500

    delete config params

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.db = {
-    host: "82.202.172.211",
-    port: "3316",
-    user: "u106227_ca",
-    database: "u106227_ce",
-    password: "bW1cI0oG4xeW7h"
+    host: "*",
+    port: "*",
+    user: "*",
+    database: "*",
+    password: "*"
 };
